7953fb4084cd250eb21a8a505d05d81b7eeafb4f
Bui Tung Linh <linh.buitung18@gmail.com>
2026-08-09 12:39:34 +0200

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        var stackTemp = new Stack<(int value, int index)>();
+        var result = new int[temperatures.Length];
+        for (var i = 0; i < temperatures.Length; i ++)
+        {
+            while (stackTemp.Count != 0 && stackTemp.Peek().value < temperatures[i])
+            {
+                var popped = stackTemp.Pop();
+                result[popped.index] = i - popped.index;
+            }
+            
+            stackTemp.Push((temperatures[i], i));
+        }
+
+        return result.ToArray();
+    }
+}
